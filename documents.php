e2c2cb2611b6984f201e74e3ae1a6caa64a24d4b
WillGolden80742 <william80742@gmail.com>
2022-03-13 21:23:24 -0300

    modificando mecanica de alteração

--- documents.php
+++ documents.php
@@ -14,7 +14,7 @@
         echo "#doc".$value["docId"]."{ background-image:url(data:image/jpeg;base64,". base64_encode($value["picture"])."); }";
         $documents.="<div class='document'><center><img src='Images/edit.png'/ id='doc".$value["docId"]."' onclick='openfile(".$value["docId"].");' ></center>";
         $documents.="<p>Documento: </br>". $value["type"]."</p><p>Hash : <br><input class='inputHash' type=text value='".$auth-> encrypt($value["picture"])."'></input>";
-        $documents.="<form action=\"uploadDocs.php?doc=".$value["docId"]."&id=".$value["docId"]."\" method=\"post\" enctype=\"multipart/form-data\"> <input id=\"editProfilePic".$value["docId"]."\" accept=\".jpeg,.jpg,.png\" style=\"display:none;\" id=\"editProfile\" type=\"file\" name=\"pic".$value["docId"]."\"> <input class=\"inputSubmit salvar".$value["docId"]."\" type=submit value=\"SALVAR\"> </form> ";
+        $documents.="<form action=\"uploadDocs.php?doc=".$value["docId"]."&id=".$value["docId"]."\" method=\"post\" enctype=\"multipart/form-data\"> <input id=\"editProfilePic".$value["docId"]."\" accept=\".jpeg,.jpg,.png\" style=\"display:none;\" onchange=\"display(".$value["docId"].");\" id=\"editProfile\" type=\"file\" name=\"pic".$value["docId"]."\"> <input class=\"inputSubmit salvar salvar".$value["docId"]."\" type=submit value=\"SALVAR\"> </form> ";
         $documents.="</div>";
     }
     echo "</style>";
