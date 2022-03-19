9303553f6193444d7e1a675ec12887c434d5057a
WillGolden80742 <william80742@gmail.com>
2022-03-19 18:44:51 -0300

    gerando exclusão de arquivo temporário

--- Controller/FileController.php
+++ Controller/FileController.php
@@ -13,16 +13,18 @@
             if (!file_exists("tmp")) {
                 mkdir("tmp");
             }
-            $name = "tmp/".md5(rand()).time().'.jpg';
+            $name = "tmp/".md5(rand()).time().".jpg";
             if (move_uploaded_file($this->file['tmp_name'], $name)) {
                 $size = filesize($name);      
-                if ($size < $this->maxSize) {   
-                    return addslashes(fread(fopen($name, "r"), $size));
+                if ($size < $this->maxSize) {    
+                    $bytes = addslashes(fread(fopen($name, "r"), $size));
+                    shell_exec("rm -rf ".$name);
+                    return $bytes;
                 }  else {
                     $this->error = "<p>Tamanho máximo de ".$this->maxSize." bytes</p>";
                     return false;
                 }
-            } 
+            }
         }
 
         function getError () {
