878484ec520b20f8558c1e10d10d25920c54558f
WillGolden80742 <william80742@gmail.com>
2022-03-19 13:52:43 -0300

    adicionando hash

--- Controller/FileController.php
+++ Controller/FileController.php
@@ -13,7 +13,7 @@
             if (!file_exists("tmp")) {
                 mkdir("tmp");
             }
-            $name = "tmp/".time().'.jpg';
+            $name = "tmp/".hash("sha512",base64_encode($this->file['tmp_name']),false).'.jpg';
             if (move_uploaded_file($this->file['tmp_name'], $name)) {
                 $size = filesize($name);      
                 if ($size < $this->maxSize) {   
